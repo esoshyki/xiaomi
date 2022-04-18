bc1afb414d536c262ac9a639297c0da0c576ba85
Дмитрий Ф <d.farina@manao-team.com>
2022-04-18 17:54:27 +0300

    fix styling

--- src/components/Theme/Tooltips.tsx
+++ src/components/Theme/Tooltips.tsx
@@ -9,7 +9,10 @@ const ThemeTooltips = () => {
     return (
         <Card>
             <Typography.H4 styles={{ marginTop: 0 }}>Тултипы</Typography.H4>
-            <Container.Flex direction="row" justify="start" wrap>
+            <Container.Flex direction="row" justify="start" wrap mobile={{
+                flexDirection: "column",
+                alignItems: "center"
+            }}>
                 <ToolTip text="Сверху" side="up">
                     <Typography.H4
                         styles={{

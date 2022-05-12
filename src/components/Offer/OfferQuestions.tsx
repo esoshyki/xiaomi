601e213021aa24fb0b373d38b5c1641e1f8aaf71
Дмитрий Ф <d.farina@manao-team.com>
2022-05-12 18:01:44 +0300

    start fixes

--- src/components/Offer/OfferQuestions.tsx
+++ src/components/Offer/OfferQuestions.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { useOfferData } from "./hooks/useOfferData";
 import Container from "../ui/Container";
 import OfferQuestion, { QuestionData } from "./Question/OfferQuestion";
 
 const OfferQuestions = () => {
-    const { getQuestion, errors, getQuestions } = useOfferData();
+    const { getQuestion, errors, getQuestions, step } = useOfferData();
 
-    const question = errors.length? null : getQuestion();
-
-    if (!question && !errors.length) {
-        getQuestions();
-    }
-
+    const question = useMemo(() => {
+        if (step === "questions") {
+            return getQuestion()
+        }
+    }, [step])
+    
     useEffect(() => {
         getQuestions()
     }, [])
